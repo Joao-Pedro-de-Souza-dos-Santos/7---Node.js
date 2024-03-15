4cd3f79117d41ec7418f880b0baad83bbf296dda
Joao-Pedro-de-Souza-dos-Santos <joaopedrodesouzadossantos15@gmail.com>
2024-03-15 20:19:11 -0300

    add controllers update and delete class 03

--- class_03 - controllers/src/controllers/userControlllers.ts
+++ class_03 - controllers/src/controllers/userControlllers.ts
@@ -6,15 +6,33 @@ export const userControllers = {
         res.json({ status: `user ${id} created!` });
 
         if (id && classes && attack) {
-            res.json({ status: `user ${id} created!`});
+            res.status(201).json({ status: `user ${id} created!`});
             return;
         }
 
-        res.json({ status: `user not created`});
+        res.status(400).json({ status: `user not created`});
     },
 
     read(req: Request, res: Response) {
         const { id } = req.params;
         res.json({ user : id });
     },
+
+    update(req: Request, res: Response) {
+        const { id } = req.params;
+        const { classes, attack } = req.body;
+
+        if ( id && classes && attack ) {
+            console.log("updated", { id, classes, attack});
+            res.json({ status: `user ${id} updated!`});
+            return;
+        }
+
+        res.status(400).json({ status: `error updated user`});
+    },
+
+    delete(req: Request, res: Response) {
+        const { id } = req.params;
+        res.status(200).json({ status: `user ${id} deleted`});
+    }
 }
